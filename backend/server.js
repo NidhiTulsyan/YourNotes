c559137fa895807ae8bca7d05fdc011aea611e33
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-09-04 16:20:16 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const connectData = require('./db');
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 // const port = process.env.PORT;
@@ -13,6 +13,6 @@ app.use(cors())
 app.use('/api/user',require('./routes/userLoginRoute'));
 app.use('/api/notes',require('./routes/notesRoute'));
 
-app.listen(PORT,()=>{
+app.listen(port,()=>{
     console.log(`server is running at port ${port} at http://localhost:${port}`);
 })
