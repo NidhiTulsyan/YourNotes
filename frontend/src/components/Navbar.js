8d93568c3ceace5633c1a7337f3f2cc94b84feaa
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-09-05 14:39:22 +0530

    changed site name

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          iNoteBook
+          UpNote
         </Link>
         <button
           className="navbar-toggler"
